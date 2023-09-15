39cfc2fccb2720654fac9eb0a354978fbcb7065f
cloudwinvps <143119906+cloudwinvps@users.noreply.github.com>
2023-09-15 17:56:11 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,48 @@
 import React, { useState, useEffect } from "react";
-
-import { createTheme, ThemeProvider } from "@material-ui/core/styles";
-
 import Routes from "./routes";
-
 import "react-toastify/dist/ReactToastify.css";
 
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import { ptBR } from "@material-ui/core/locale";
 
-import "@fontsource/oxanium";
-
-const theme = createTheme({
-
-  typography: {
-    fontFamily: 'Oxanium',
-  },
-
-  scrollbarStyles: {
-    '&::-webkit-scrollbar': {
-      width: '10px',
-      height: '10px',
-    },
-    '&::-webkit-scrollbar-thumb': {
-      boxShadow: 'inset 0 0 8px rgba(0, 0, 0, 5)',
-      backgroundColor: '#223B62',
-    },
-  },
-  
-  palette: {
-    primary: { main: '#223B62' },
-    danger: { main: '#1172b8' },
-  },
-
-});
-
 const App = () => {
+	const [locale, setLocale] = useState();
 
-  const [locale, setLocale] = useState();
-
-  useEffect(() => {
-    const i18nlocale = localStorage.getItem("i18nextLng");
-    const browserLocale = 
-      i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
-    
-    if (browserLocale === "ptBR") {
-      setLocale(ptBR); 
-    }
-  }, []);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+  const theme = createTheme(
+    {
+      scrollbarStyles: {
+        '&::-webkit-scrollbar': {
+          width: '8px',
+          height: '8px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          boxShadow: 'inset 0 0 6px rgba(0, 0, 0, 0.3)',
+          backgroundColor: '#e8e8e8',
+        },
+      },
+      palette: {
+        primary: { main: '#29A71A' },
+        danger: { main: '#1172b8' },
+      },
+    },
+    locale
   );
+
+	useEffect(() => {
+		const i18nlocale = localStorage.getItem("i18nextLng");
+		const browserLocale =
+			i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
+
+		if (browserLocale === "ptBR") {
+			setLocale(ptBR);
+		}
+	}, []);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Routes />
+		</ThemeProvider>
+	);
 };
 
 export default App;
